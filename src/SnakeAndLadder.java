496c5db1cbe1e5634b94606f19145e7a4545e4b0
its-shreybhatt <shreybhatt2@gmail.com>
2023-02-24 20:30:01 +0530

    Initialize : Player Position at 0

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,5 +1,7 @@
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        System.out.println("Welcome to Snake And Ladder Program");
+        System.out.println("Welcome to Snake Ladder Program");
+        int position = 0;
+        System.out.println("Position = "+position);
     }
 }
