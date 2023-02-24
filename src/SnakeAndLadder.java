7303efeaacddb5c692450a62ae0036292a9d39df
its-shreybhatt <shreybhatt2@gmail.com>
2023-02-24 20:34:27 +0530

    Added : Code to obtain Random value on Dice

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,7 +1,12 @@
+import java.util.Random;
+
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake Ladder Program");
         int position = 0;
         System.out.println("Position = "+position);
+        Random value = new Random();
+        int dice = value.nextInt(6)+1;
+        System.out.println("Random number is "+dice);
     }
 }
