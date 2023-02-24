209ba5eb1c70ef98bf66ae448f66373eca00d9b3
its-shreybhatt <shreybhatt2@gmail.com>
2023-02-24 22:06:26 +0530

    Added : Code to count Number of time Dice was Rolled

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -4,11 +4,13 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake Ladder Program");
         int position = 0;
+        int count = 0;
         System.out.println("Initial Position = " + position);
         Random value = new Random();
         while (position < 100) {
             int dice = value.nextInt(6) + 1;
             System.out.println("dice pointing : " + dice);
+            count = count + 1;
 
             int choose = value.nextInt(3);
             if (choose == 0) {
@@ -23,7 +25,8 @@ public class SnakeAndLadder {
                     position = 0;
             } else
                 position = position;
-            System.out.println("Current Position = " + position);
+            System.out.println("winner !! " + position + "/100");
+            System.out.println("no of times dice rolled : " + count);
         }
     }
 }
