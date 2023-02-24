d5ca24fde5894cfee7c5192cc4e62bcdcaec0b31
its-shreybhatt <shreybhatt2@gmail.com>
2023-02-24 20:50:19 +0530

    Added : While Loop for player position >=100

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -4,21 +4,24 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake Ladder Program");
         int position = 0;
-        System.out.println("Initial Position = "+position);
+        System.out.println("Initial Position = " + position);
         Random value = new Random();
-        int dice = value.nextInt(6)+1;
-        System.out.println("dice pointing : " + dice);
+        while (position < 100) {
+            int dice = value.nextInt(6) + 1;
+            System.out.println("dice pointing : " + dice);
 
-        int choose = value.nextInt(3);
-        if (choose == 0) {
-            //ladder
-                    position = position + dice;
-            }
-        else if (choose == 1) {
-            //snake
-                    position = position - dice;
-        } else
-            position = position;
-        System.out.println("Current Position = " + position);
+            int choose = value.nextInt(3);
+            if (choose == 0) {
+                //ladder
+                position = position + dice;
+            } else if (choose == 1) {
+                //snake
+                position = position - dice;
+                if (position < 0)
+                    position = 0;
+            } else
+                position = position;
+            System.out.println("Current Position = " + position);
+        }
     }
 }
