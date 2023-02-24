a2fdae7562f8a0b86bac962e19343c9893e83572
its-shreybhatt <shreybhatt2@gmail.com>
2023-02-24 22:21:27 +0530

    Modified : By Introducing Second Player to the Game

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -3,30 +3,60 @@ import java.util.Random;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake Ladder Program");
-        int position = 0;
+        int positionOne = 0;
+        int positionTwo = 0;
         int count = 0;
-        System.out.println("Initial Position = " + position);
-        Random value = new Random();
-        while (position < 100) {
-            int dice = value.nextInt(6) + 1;
-            System.out.println("dice pointing : " + dice);
-            count = count + 1;
 
-            int choose = value.nextInt(3);
-            if (choose == 0) {
-                //ladder
-                position = position + dice;
-                if (position > 100)
-                    position = position - dice;
-            } else if (choose == 1) {
-                //snake
-                position = position - dice;
-                if (position < 0)
-                    position = 0;
-            } else
-                position = position;
-            System.out.println("winner !! " + position + "/100");
-            System.out.println("no of times dice rolled : " + count);
+        while (positionOne < 100 && positionTwo < 100) {
+            count = count + 1;
+            Random value = new Random();
+            int dice = value.nextInt(6) + 1;
+            int choice = value.nextInt(2);
+
+            if (choice == 0) {
+                // player 1
+                int choose = value.nextInt(3);
+                System.out.println("dice pointing for player 1 : " + dice);
+
+                if (choose == 0) {
+                    System.out.println("ladder");
+                    positionOne = positionOne + dice;
+                    if (positionOne > 100)
+                        positionOne = positionOne - dice;
+                } else if (choose == 1) {
+                    System.out.println("snake");
+                    positionOne = positionOne - dice;
+                    if (positionOne < 0)
+                        positionOne = 0;
+                } else System.out.println("no play");
+                positionOne = positionOne;
+
+                System.out.println("1st player position = " + positionOne);
+            } else {
+
+                //player 2
+                int choose = value.nextInt(3);
+                System.out.println("dice pointing for player 2 : " + dice);
+                if (choose == 0) {
+                    System.out.println("ladder");
+                    positionTwo = positionTwo + dice;
+                    if (positionTwo > 100)
+                        positionTwo = positionTwo - dice;
+                } else if (choose == 1) {
+                    System.out.println("snake");
+                    positionTwo = positionTwo - dice;
+                    if (positionTwo < 0)
+                        positionTwo = 0;
+                } else System.out.println("no play");
+                positionTwo = positionTwo;
+
+                System.out.println("2nd player position = " + positionTwo);
+            }
         }
+        // Output
+        System.out.println("no of times dice rolled : " + count);
+        if (positionOne == 100) {
+            System.out.println("winner player 1 !! " + positionOne + "/100");
+        } else System.out.println("winner player 2 !! " + positionTwo + "/100");
     }
 }
